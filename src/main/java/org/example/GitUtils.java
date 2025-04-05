dab4b882dea6342948b34eceea1d0dfa3d15e455
Rade Pejanović <radepejanovic@gmail.com>
2025-04-05 19:03:54 +0200

    feat: add git diff --name-only command function wrapper

--- src/main/java/org/example/GitUtils.java
+++ src/main/java/org/example/GitUtils.java
@@ -21,6 +21,15 @@ public class GitUtils {
             return readOutput(process.getInputStream());
     }
 
+    public List<String> diffNameOnly(String commit1, String commit2) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "--name-only", commit1, commit2);
+        processBuilder.directory(repoPath);
+
+        Process process = processBuilder.start();
+
+        return readOutput(process.getInputStream());
+    }
+
     private List<String> readOutput(InputStream inputStream) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             return reader.lines().collect(Collectors.toList());
